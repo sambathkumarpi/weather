c72ed461778664470d25df70f1ba16776759aeb7
sambath <sambathkumarpi@gmail.com>
2025-02-13 14:15:28 +0100

    Create create.tf

---
+++ create.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "4.0.34"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "google" {
+  project = "my-gcp-project"
+  region  = "us-central1"
+}
